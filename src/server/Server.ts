7fd9bd42cf8f3ad293cb400cc103df5584445339
Lucas Alfeu <a1feu@outlook.com>
2026-03-11 22:39:13 -0300

    FEAT: arquivo de rotas do projeto

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import { router } from './routes/index.js';
 
 const server = express();
 
-server.get('/', (_, res) => {
-  return res.send('Olá, DEV!')
-})
+server.use(express.json())
+server.use(router)
 
 export { server };
