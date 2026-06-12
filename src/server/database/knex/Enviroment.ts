bc0c901406721efbafbe850e3d7d9799c58b179e
Lucas Alfeu <a1feu@outlook.com>
2026-06-12 10:47:50 -0300

    FEAT: Coinfigurações para enviar os dados para o mysql

--- src/server/database/knex/Enviroment.ts
+++ src/server/database/knex/Enviroment.ts
@@ -1,24 +1,40 @@
 import { Knex } from 'knex'
 import path from 'path'
 
+// export const development: Knex.Config = { 
+//   client: 'sqlite3',
+//   useNullAsDefault: true,
+//   connection: {
+//     filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
+//   },
+//   migrations: {
+//     directory: path.resolve(__dirname, '..', 'migrations')
+//   },
+//   seeds: {
+//     directory: path.resolve(__dirname, '..', 'seeds')
+//   },
+//   pool: {
+//     afterCreate: (connection: any, done: Function) => {
+//       connection.run('PRAGMA foreign_keys = ON')
+//       done();
+//     }
+//   }
+// }
+
 export const development: Knex.Config = { 
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
-  },
+  client: 'mysql2',
   migrations: {
     directory: path.resolve(__dirname, '..', 'migrations')
   },
   seeds: {
     directory: path.resolve(__dirname, '..', 'seeds')
   },
-  pool: {
-    afterCreate: (connection: any, done: Function) => {
-      connection.run('PRAGMA foreign_keys = ON')
-      done();
-    }
-  }
+  connection: {
+    host: process.env.BD_HOST as string,
+    user: process.env.BD_USER as string,
+    password: process.env.BD_PASSWORD as string,
+    database: process.env.BD_NAME as string,
+  },
 }
 
 export const test: Knex.Config = { 
@@ -28,4 +44,10 @@ export const test: Knex.Config = {
 
 export const production: Knex.Config = {
   ...development,
- }
+  connection: {
+    host: process.env.BD_HOST as string,
+    user: process.env.BD_USER as string,
+    password: process.env.BD_PASSWORD as string,
+    database: process.env.BD_NAME as string,
+  }
+}
