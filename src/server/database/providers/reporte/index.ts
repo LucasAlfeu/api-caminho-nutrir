8f8048df18b1c7bf8dbd95c02cba8243412b5826
Lucas Alfeu <a1feu@outlook.com>
2026-08-04 11:39:19 -0300

    FEAT(reporte): exportando os métodos da provider

---
+++ src/server/database/providers/reporte/index.ts
@@ -0,0 +1,10 @@
+import { create } from "./Create";
+import { deleteById } from "./DeleteById";
+import { getAllById } from "./GetAllById";
+
+
+export const ReporteProvidedr = {
+  create: create,
+  getAllById: getAllById,
+  deleteById: deleteById
+}
