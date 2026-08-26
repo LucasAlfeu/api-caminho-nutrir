3cb6dde3b93bf3179e473ace1d090fa8b9dc8476
Lucas Alfeu <a1feu@outlook.com>
2026-08-26 00:41:43 -0300

    mensagem 5

--- src/server/database/providers/estacao/GetById.ts
+++ src/server/database/providers/estacao/GetById.ts
@@ -1,4 +1,3 @@
-// database/providers/estacao/getById.ts (ou onde estiver seu provider)
 import { Knex } from "../../knex"
 import { IEstacao } from "../../models";
 import { ETableNames } from "../../ETable";
@@ -6,22 +5,18 @@ import { ETableNames } from "../../ETable";
 export const getById = async (id: number): Promise<IEstacao | Error> => {
   try {
     const result = await Knex(ETableNames.estacao)
-      .select('*')
+      .select("*", Knex.raw('"fk_Classificacao_id" as "idClassificacao"'))
       .where('id', '=', id)
       .first();
     
-    if(result) {
-      const { fk_Classificacao_id, ...restoDoBanco } = result as any;
-      
-      return {
-        ...restoDoBanco,
-        idClassificacao: fk_Classificacao_id // Transformando para a sua interface
-      };
+    if (result) {
+      // Como já renomeamos no select com Knex.raw, a propriedade idClassificacao já vem pronta
+      return result as IEstacao;
     }
 
     return new Error('Registro não encontrado');
   } catch (error) {
-    console.log(error);
+    console.error("ERRO NO GET BY ID:", error);
     return new Error('Registro não encontrado');
   }
-}
+};
